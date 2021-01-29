64c53d5ce0e1ad19540a29598162ebfcde392573
null <60427892+vierofernando@users.noreply.github.com>
2021-01-29 14:59:43 +0700

    improved index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const giveMeAJoke = require("discord-jokes");
-const fetch = require("node-fetch");
 const devbreh = require('awesome-dev-jokes')
 const artificialIntelligence = require('./ai.js');
 const translate = require('@iamtraction/google-translate');
-
 const randomPuppy = require('random-puppy');
 const insulter = require('insult');
-const fs = require('fs')
 process
   .on('unhandledRejection', (reason, p) => {
     console.error(reason, 'Unhandled Rejection at Promise', p);
@@ -18,6 +15,7 @@ process
   process.on('error', (err) => {
     console.error('whoops! there was an error');
   });
+
 module.exports = {
     random:{
     cnjoke: function cnjoke() {
@@ -28,7 +26,7 @@ module.exports = {
                 })
             })
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     joke: function joke() {
@@ -39,7 +37,7 @@ module.exports = {
                 })
             })
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     devjoke: function devjoke() {
@@ -50,7 +48,7 @@ module.exports = {
                 resolve(jokesboi)
             })
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     insult: async () => {
@@ -69,120 +67,97 @@ image: {
         try {
             const num = Math.floor(Math.random() * 20000) + 1;
             const padded = num.toString().padStart(7, "0");
-            const vase = `http://thisvesseldoesnotexist.s3-website-us-west-2.amazonaws.com/public/v2/fakes/${padded}.jpg`
-            return vase;
+            return `http://thisvesseldoesnotexist.s3-website-us-west-2.amazonaws.com/public/v2/fakes/${padded}.jpg`;
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     cat: async () => {
         try {
-            const subReddits = ["cat"]
-            const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-            const img = await randomPuppy(random)
-            return img
+            const img = await randomPuppy("cat");
+            return img;
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     dog: async () => {
         try {
-            const subReddits = ["dog", "puppy"]
-            const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-            const img = await randomPuppy(random)
-            return img
+            const img = await randomPuppy(["dog", "puppy"][Math.floor(Math.random() * 2)]);
+            return img;
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     duck: async () => {
         try {
-            const subReddits = ["duck"]
-            const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-            const img = await randomPuppy(random)
-            return img
+            const img = await randomPuppy("duck");
+            return img;
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     aww: async () => {
         try {
-            const subReddits = ["aww"]
-            const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-            const img = await randomPuppy(random)
-            return img
+            const img = await randomPuppy("aww");
+            return img;
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     meme: async () => {
-        try {
-            const subReddits = ["memes"]
-            const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-            const img = await randomPuppy(random)
-            return img
+        try 
+            const img = await randomPuppy("memes");
+            return img;
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     dankmeme: async () => {
         try {
-            const subReddits = ["dankmemes"]
-            const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-            const img = await randomPuppy(random)
-            return img
+            const img = await randomPuppy("dankmemes");
+            return img;
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     facepalm: async () => {
         try {
-            const subReddits = ["facepalm"]
-            const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-            const img = await randomPuppy(random)
-            return img
+            const img = await randomPuppy("facepalm");
+            return img;
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     hpmeme: async () => {
         try {
-            const subReddits = ["harrypottermemes"]
-            const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-            const img = await randomPuppy(random)
-            return img
+            const img = await randomPuppy("harrypottermemes");
+            return img;
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     wholesome: async () => {
         try {
-            const subReddits = ["wholesomememes"]
-            const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-            const img = await randomPuppy(random)
-            return img
+            const img = await randomPuppy("wholesomememes");
+            return img;
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     art: async () => {
         try {
-            const subReddits = ["art"]
-            const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-            const img = await randomPuppy(random)
+            const img = await randomPuppy("art");
             return img
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
     deadinside: async () => {
         try {
-            const subReddits = ["WatchPeopleDieInside"]
-            const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-            const img = await randomPuppy(random)
+            const img = await randomPuppy("WatchPeopleDieInside");
             return img
         } catch (err) {
-            throw new Error("An error occurred , error: " + err);
+            throw new Error("An error occurred, error: " + err);
         }
     },
 },
@@ -191,25 +166,23 @@ ai: {
     if(!message) {
         throw new TypeError('Message Cannot be empty in getReply()')
       }
-      if(!language) {
-        console.log("No language has been provided defaulting to english as destination language")
-        try{
-          const text = await artificialIntelligence(message)
-          const trans = await translate(text, { to: 'en' })
-          return trans.text
-        }catch(err){
-          console.error(err)
+      if (!language) {
+        console.warn("No language has been provided, defaulting to english as destination language");
+        try {
+          const text = await artificialIntelligence(message);
+          const trans = await translate(text, { to: 'en' });
+          return trans.text;
+        } catch (err) {
+          console.error(err);
         }
       }
-      try{
+      try {
         const text = await artificialIntelligence(message)
         const trans = await translate(text, { to: language })
         return trans.text
-      }catch(err){
-        console.error(err)
+      } catch (err) {
+        console.error(err);
       }
-
    }
 }
 }
-
