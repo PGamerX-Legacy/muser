a60c75791b7cb596837665f86586fe52a4edbffd
PGamerX <websitedesigner123456@gmail.com>
2021-04-11 21:08:54 +0530

    Create node.js

---
+++ examples/javascript/jokes/node.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+// Use node-fetch as the package
+
+let res = await fetch('https://api.pgamerx.com/joke/any?api_key=something")
+// Fetch The API
+           
+let json = await res.json()
+// Convert it into JSON
+
+let joke = json[0]
+// Get joke as variable
+
+console.log(joke)
+// Log The joke
