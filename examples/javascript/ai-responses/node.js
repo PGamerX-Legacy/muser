037a2de78d7331387c34b769580440496110fa4a
PGamerX <websitedesigner123456@gmail.com>
2021-04-24 12:27:18 +0530

    Update node.js

--- examples/javascript/ai-responses/node.js
+++ examples/javascript/ai-responses/node.js
@@ -1,14 +1,6 @@
-const fetch = require('node-fetch');
-// Use node-fetch as the package
+let stuff =  require("random-stuff-api")
+let rsa = new stuff({
+    key: "api_key" // Optional 
+})
 
-let res = await fetch("https://api.pgamerx.com/ai/response?api_key=something&message=Hey&language=en")
-// Fetch The API
-           
-let json = await res.json()
-// Convert it into JSON
-
-let response = json[0]
-// Get joke as variable
-
-console.log(response)
-// Log The response
+let response = await rsa.ai(message, language)
