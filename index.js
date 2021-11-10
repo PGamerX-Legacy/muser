75a333bae3a6e2a366dd7b1837a5e66389151e61
Peipr <peipr@peiphy.xyz>
2021-11-10 23:15:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js');
 require("dotenv").config();
-let token = process.env.BOTOKEN
+const token = process.env.BOTOKEN
 const manager = new ShardingManager('./bot.js', { token: token, totalShards: 0 });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
