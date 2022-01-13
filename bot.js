2be4545139f5cdce21ad10aea71cba4d1a788a46
Peipr <peipr@peiphy.xyz>
2022-01-13 16:36:22 +0100

    TF?

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ const topGGToken = process.env.TOPGG_TOKEN;
 const logdna = require('@logdna/logger');
 const options = {app: 'muser', level: 'debug'};
 const logger = logdna.createLogger(logdnakey, options);
-logger.log('Hello world!', 'info');
+////logger.log('Hello world!', 'info');
 const cronitor = require("cronitor")(cronitorID);
 const monitor = new cronitor.Monitor("Muser");
 /////////////////////////////////////////////////////////
@@ -31,7 +31,7 @@ const Topgg = require("@top-gg/sdk");
 const { AutoPoster } = require("topgg-autoposter");
 const webhook = new Topgg.Webhook(topGGAuth);
 const ap = AutoPoster(topGGToken, client);
-logger.info("All packages imported. Credentials set."); console.log("All packages imported. Credentials set.");
+///logger.info("All packages imported. Credentials set."); console.log("All packages imported. Credentials set.");
 /*/////////////////////////////////////////////////////////////////////////////////////////////////////////////
                                                                         All the
 ugly constants related to DJS
@@ -87,7 +87,7 @@ app.post(
           `Thanks for voting <:love:736194392305434704>:. You can now access "FILTER" commands in the bot for next 1 week, More perks soon! `
         );
       user.send({ embeds: [thanks2] });
-      logger.info(`${user_info.username}#${user_info.discriminator} voted`)
+      /////logger.info(`${user_info.username}#${user_info.discriminator} voted`)
     });
     client.channels.fetch("783581253001150494").then(async (channel) => {
       let user_info = await client.users.fetch(vote.user);
@@ -104,9 +104,9 @@ app.post(
   })
 );
 ap.on("posted", () => {
-  logger.info("Posted stats to Top.GG!");console.log("Posted stats to Top.gg!");
+  /////logger.info("Posted stats to Top.GG!");console.log("Posted stats to Top.gg!");
 });
-logger.error("Vote bug still not fixed")
+/////logger.error("Vote bug still not fixed")
 /*/////////////////////////////////////////////////////////////////////////////////////////////////////////////
                                                                         This is
 (supposed to be) the command parser
