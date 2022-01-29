c590d2d912e677b9e9f96af2aa38d9ee5333558e
Restyled.io <commits@restyled.io>
2022-01-29 13:22:18 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const token = process.env.BOTOKEN
 
-const { ShardingManager } = require('discord.js');
+const {ShardingManager} = require('discord.js');
 
-const manager = new ShardingManager('./bot.js', { token: token });
+const manager = new ShardingManager('./bot.js', {token : token});
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
