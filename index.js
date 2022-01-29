046f52825a7b2607c5c148c64d441e70b3e69469
Restyled.io <commits@restyled.io>
2022-01-29 13:22:20 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-const token = process.env.BOTOKEN
+const token = process.env.BOTOKEN;
 
-const {ShardingManager} = require('discord.js');
+const { ShardingManager } = require("discord.js");
 
-const manager = new ShardingManager('./bot.js', {token : token});
+const manager = new ShardingManager("./bot.js", { token: token });
 
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
 
 manager.spawn();
