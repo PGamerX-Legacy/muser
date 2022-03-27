83d7c517a92d01cde2c1dfea7c1aaff09450d7a7
PGamerX <websitedesigner123456@gmail.com>
2022-03-27 18:05:09 +0530

    Add Voting Requirement

--- scommands/autoplay.js
+++ scommands/autoplay.js
@@ -1,26 +1,48 @@
 // noinspection DuplicatedCode
 
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const db = require('quick.db')
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const db = require("quick.db");
+const { MessageEmbed } = require("discord.js");
+const userinfo = require("../models/user.js");
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('autoplay')
-        .setDescription('Toggles autoplay feature in Muser')
-                    ,
-	async execute(interaction) {
+  data: new SlashCommandBuilder()
+    .setName("autoplay")
+    .setDescription("Toggles autoplay feature in Muser"),
+  async execute(interaction) {
+    const data = await userinfo.findOne({ UserID: user_id });
+    if (!data) {
+      const embed = new MessageEmbed()
+        .setTitle("Forbidden")
+        .setDescription(
+          "You are not allowed to use this command! Only people who have **voted** for the bot within the last week can use this command! (**Voting is free**)"
+        )
+        .setColor("RED")
+        .addFields({
+          name: "Vote for the bot!",
+          value:
+            "[Vote for Muser on top.gg](https://top.gg/bot/763418289689985035/vote) ",
+        });
 
-        let distube;
-        const is_premium = await db.get(`PREMIUM_${interaction.guild.id}`)
-        if(is_premium === "yes"){
-            distube = interaction.client.premium_distube
-        }else{
-            distube = interaction.client.distube
-        }
-           if(!interaction.member.voice.channel) {await interaction.reply({content: "You are not in a voice channel!"})
-return}
+      return interaction.reply({
+        embeds: [embed],
+      });
+    } else if (data.voter == true) {
+      let distube;
+      const is_premium = await db.get(`PREMIUM_${interaction.guild.id}`);
+      if (is_premium === "yes") {
+        distube = interaction.client.premium_distube;
+      } else {
+        distube = interaction.client.distube;
+      }
+      if (!interaction.member.voice.channel) {
+        await interaction.reply({ content: "You are not in a voice channel!" });
+        return;
+      }
 
-const mode = distube.toggleAutoplay(interaction)
-await interaction.reply("Setting autoplay mode to `" + (mode ? "On" : "Off") + "`")
-
-	},
+      const mode = distube.toggleAutoplay(interaction);
+      await interaction.reply(
+        "Setting autoplay mode to `" + (mode ? "On" : "Off") + "`"
+      );
+    }
+  },
 };
--- scommands/filters.js
+++ scommands/filters.js
@@ -1,18 +1,8 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
 const userinfo = require("../models/user.js");
 const db = require("quick.db");
-async function isVoter(user_id) {
-  if (user_id) {
-    const userdetail = await userinfo.findOne({
-      UserID: user_id,
-    });
-    if (!userdetail) {
-      return false;
-    } else if (userdetail) {
-      return true;
-    }
-  }
-}
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("filters")
@@ -41,38 +31,48 @@ module.exports = {
     ),
   async execute(interaction) {
     const user_id = interaction.user.id;
-  /*  const VOTER = await isVoter(user_id);
-    if (!VOTER) {
-      return await interaction.reply(
-        `Filter command is only available for those who have voted for me (voting is free) or for those who have purchased the premium version. You can vote for me: <https://u.pgamerx.com/vote/muser>`
-      );
-    }
-  */
-    let distube;
-    const is_premium = await db.get(`PREMIUM_${interaction.guild.id}`);
-    if (is_premium === "yes") {
-      distube = interaction.client.premium_distube;
-    } else {
-      distube = interaction.client.distube;
-    }
-    let input = interaction.options.getString("filter");
-    if (!interaction.member.voice.channel) {
-      await interaction.reply({ content: "You are not in a voice channel!" });
-      return;
-    }
 
-    const queue = distube.getQueue(interaction);
-    if (!queue) {
-      await interaction.reply(`No song is playing!`);
+    const data = await userinfo.findOne({ UserID: user_id });
+    if (!data) {
+      const embed = new MessageEmbed()
+      .setTitle("Forbidden")
+      .setDescription("You are not allowed to use this command! Only people who have **voted** for the bot within the last week can use this command! (**Voting is free**)")
+      .setColor("RED")
+      .addFields({
+        name: "Vote for the bot!",
+        value: "[Vote for Muser on top.gg](https://top.gg/bot/763418289689985035/vote) ",
+      })
+
+      return interaction.reply({
+        embeds: [embed]
+      })
+    } else if (data.voter == true) {
+      let distube;
+      const is_premium = await db.get(`PREMIUM_${interaction.guild.id}`);
+      if (is_premium === "yes") {
+        distube = interaction.client.premium_distube;
+      } else {
+        distube = interaction.client.distube;
+      }
+      let input = interaction.options.getString("filter");
+      if (!interaction.member.voice.channel) {
+        await interaction.reply({ content: "You are not in a voice channel!" });
+        return;
+      }
+
+      const queue = distube.getQueue(interaction);
+      if (!queue) {
+        await interaction.reply(`No song is playing!`);
+      }
+      if (input === "disable") {
+        await interaction.reply(`🎵 Disabled all the filters from the queue`);
+        await distube.setFilter(queue, false);
+        return;
+      }
+      await interaction.reply(
+        `🎵 Applying ${input} filter on the current queue `
+      );
+      await distube.setFilter(queue, input);
     }
-    if (input === "disable") {
-      await interaction.reply(`🎵 Disabled all the filters from the queue`);
-      await distube.setFilter(queue, false);
-      return;
-    }
-    await interaction.reply(
-      `🎵 Applying ${input} filter on the current queue `
-    );
-    await distube.setFilter(queue, input);
   },
 };
