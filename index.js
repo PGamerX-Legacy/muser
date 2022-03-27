e6c3b7bf7dd506bc07aef95ebdb7f3373fb91856
PGamerX <websitedesigner123456@gmail.com>
2022-03-27 17:13:20 +0530

    Hopefully fix the bug or I will die (#52)

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 require("dotenv").config();
-const token = process.env.BOTOKEN
+const token = process.env.BOTOKEN;
 
-const { ShardingManager } = require('discord.js');
+const topGGToken = process.env.TOPGG_TOKEN;
 
-const manager = new ShardingManager('./bot.js', { token: token });
+const Topgg = require("@top-gg/sdk");
+const express = require("express");
 
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+const { AutoPoster } = require("topgg-autoposter");
+
+const app = express();
+const webhook = new Topgg.Webhook("MuserIsAnAmazingBot");
+
+const { ShardingManager } = require("discord.js");
+
+const manager = new ShardingManager("./bot.js", { token: token });
+
+const userinfo = require("./models/user.js");
+
+manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
+
+const ap = AutoPoster(topGGToken, manager);
+ap.on("posted", () => {
+  // logger.info("Posted stats to Top.GG!");
+  console.log("Posted stats to Top.gg!");
+});
+
+
+app.post(
+  "/muserTOPGG",
+  webhook.listener(async (vote) => {
+    console.log(vote);
+    // vote will be your vote object, e.g
+    console.log(
+      `${vote.user} just voted and I sure hope this works because if not, I will destroy the server.`
+    );
+
+    const newData = new userinfo({
+      UserID: "${vote.user}",
+      voter: true,
+    });
+    await newData.save();
+
+    // Trying to eval now
+    manager.broadcastEval(
+      `
+     const user = this.users.fetch("${vote.user}").then(user => {
+     user.send("Thank you so much for voting! You can now access the filter command!");
+     })
+     `,
+      { shard: 0 }
+    );
+    // You can also throw an error to the listener callback in order to resend the webhook after a few seconds
+  })
+);
+
+app.listen(9876);
 
 manager.spawn();
+
