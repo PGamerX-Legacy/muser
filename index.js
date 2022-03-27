1d97d63440ca186c6440e5a6db102fa8eb09117e
Restyled.io <commits@restyled.io>
2022-03-27 11:38:08 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const topGGToken = process.env.TOPGG_TOKEN;
 const Topgg = require("@top-gg/sdk");
 const express = require("express");
 
-const {AutoPoster} = require("topgg-autoposter");
+const { AutoPoster } = require("topgg-autoposter");
 
 const app = express();
 const webhook = new Topgg.Webhook("MuserIsAnAmazingBot");
 
-const {ShardingManager} = require("discord.js");
+const { ShardingManager } = require("discord.js");
 
-const manager = new ShardingManager("./bot.js", {token : token});
+const manager = new ShardingManager("./bot.js", { token: token });
 
 const userinfo = require("./models/user.js");
 
@@ -25,28 +25,34 @@ ap.on("posted", () => {
   console.log("Posted stats to Top.gg!");
 });
 
-app.post("/muserTOPGG", webhook.listener(async (vote) => {
-  console.log(vote);
-  // vote will be your vote object, e.g
-  console.log(`${
-      vote.user} just voted and I sure hope this works because if not, I will destroy the server.`);
+app.post(
+  "/muserTOPGG",
+  webhook.listener(async (vote) => {
+    console.log(vote);
+    // vote will be your vote object, e.g
+    console.log(
+      `${vote.user} just voted and I sure hope this works because if not, I will destroy the server.`
+    );
 
-  const newData = new userinfo({
-    UserID : "${vote.user}",
-    voter : true,
-  });
-  await newData.save();
+    const newData = new userinfo({
+      UserID: "${vote.user}",
+      voter: true,
+    });
+    await newData.save();
 
-  // Trying to eval now
-  manager.broadcastEval(`
+    // Trying to eval now
+    manager.broadcastEval(
+      `
      const user = this.users.fetch("${vote.user}").then(user => {
      user.send("Thank you so much for voting! You can now access the filter command!");
      })
      `,
-                        {shard : 0});
-  // You can also throw an error to the listener callback in order to resend the
-  // webhook after a few seconds
-}));
+      { shard: 0 }
+    );
+    // You can also throw an error to the listener callback in order to resend the
+    // webhook after a few seconds
+  })
+);
 
 app.listen(9876);
 
